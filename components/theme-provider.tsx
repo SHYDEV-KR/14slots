d457d6c77c0874175786661da316b6050c86382a
SHYDEV <ekdmbzzang@naver.com>
2025-02-01 14:19:59 +0900

    Refactor theme provider to support more flexible configuration

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,15 +1,11 @@
 "use client"
 
-import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from "next-themes"
 import * as React from "react"
 
-export function ThemeProvider({ children }: { children: React.ReactNode }) {
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
-    <NextThemesProvider
-      attribute="class"
-      defaultTheme="dark"
-      enableSystem={false}
-    >
+    <NextThemesProvider {...props}>
       {children}
     </NextThemesProvider>
   )
